ac792014599308a30f81b092e06cf2670903f962
Leslie Alexander Sinchiguano Saltos <69779736+lsinchiguanos@users.noreply.github.com>
2021-08-01 16:36:28 -0500

    finales
    aaaaa

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,7 +3,8 @@ const { Router } = require('express');
 const router = Router();
 
 // Agregamos el archivo con la ruta que deseamos utilizar
-const { searchBook, updBook, newBook, delBook, getBooks } = require('../src/controllers/book');  // ruta de los métodos de book
+const { getBooks, searchBook, newBook, updBook, delBook } = require('../src/controllers/book');  // ruta de los métodos de book
+const { getClientes, searchCliente, newCliente, delCliente, updCliente } = require('../src/controllers/cliente');  // ruta de los métodos de cliente
 
 // Asignamos las rutas con sus subfijos y métodos a invocar
 router.get('/books', getBooks);                 // Obtiene todos los libros
@@ -11,5 +12,12 @@ router.get('/book', searchBook);                // Busca los libros según los p
 router.post('/book', newBook);                  // Registra un nuevo libro
 router.delete('/book/:idBook', delBook);        // Elimina un libro por idBook
 router.put('/book/:idBook', updBook);           // Actualiza un libro por idBook
+
+// Asignamos las rutas con sus subfijos y métodos a invocar
+router.get('/clientes', getClientes);                 // Obtiene todos los libros
+router.get('/cliente/:dni', searchCliente);                // Busca los libros según los parámetros
+router.post('/cliente', newCliente);                  // Registra un nuevo libro
+router.delete('/cliente/:idCliente', delCliente);        // Elimina un libro por idBook
+router.put('/cliente/:idCliente', updCliente);           // Actualiza un libro por idBook
 
 module.exports = { router }
